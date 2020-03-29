da9abfc5d129f86265587a90a144cf9f912f1125
Eduardo <53496439+eduardbuzzi@users.noreply.github.com>
2020-03-29 20:43:33 -0300

    Update sender-chat.sh

--- sender-chat.sh
+++ sender-chat.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+trap '' 2
+
 echo
 read -p "Informe o IP que está escutando: " IPlistening
 read -p "Informe a porta para escutar (entre 1-65535): " PORTA
 echo "Tentando se comunicar com a porta $PORTA do $IPlistening"
 echo
+trap 2
 nc $IPlistening $PORTA
 ./execute.sh
 exit
