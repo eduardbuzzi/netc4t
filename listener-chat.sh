8dcd32cc052b652208cd77d4d86e2ecb9925efac
Eduardo <53496439+eduardbuzzi@users.noreply.github.com>
2020-03-29 03:22:50 -0300

    Update listener-chat.sh

--- listener-chat.sh
+++ listener-chat.sh
@@ -1,1 +1,48 @@
+#!/bin/bash
 
+curl -s -C - -o IP http://meuip.net.br
+IPexterno=$(grep "<title>" IP | cut -d ' ' -f9 | cut -d '<' -f1)
+IPinterno=$(hostname -I | cut -d ' ' -f1)
+rm IP
+
+echo
+echo "Seu IP Interno é: $IPinterno"
+echo "Seu IP Externo é: $IPexterno"
+echo
+echo "(1) Utilizar IP Interno"
+echo "(2) Utilizar IP Externo"
+read -p "Qual sua escolha? " ESCOLHA
+case $ESCOLHA in
+1)
+./listener-chat-interno.sh
+;;
+2)
+./listener-chat-externo.sh
+;;
+*)
+echo 'curl -s -C - -o IP http://meuip.net.br' > .listener-chat-again.sh
+echo 'IPexterno=$(grep "<title>" IP | cut -d ' ' -f9 | cut -d '<' -f1)' >> .listener-chat-again.sh
+echo 'IPinterno=$(hostname -I | cut -d ' ' -f1)' >> .listener-chat-again.sh
+echo 'rm IP' >> .listener-chat-again.sh
+echo 'echo' >> .listener-chat-again.sh
+echo 'echo "Seu IP Interno é: $IPinterno"' >> .listener-chat-again.sh
+echo 'echo "Seu IP Externo é: $IPexterno"' >> .listener-chat-again.sh
+echo 'echo' >> .listener-chat-again.sh
+echo 'echo "(1) Utilizar IP Interno"' >> .listener-chat-again.sh
+echo 'echo "(2) Utilizar IP Externo"' >> .listener-chat-again.sh
+echo 'read -p "Qual sua escolha? " ESCOLHA' >> .listener-chat-again.sh
+echo 'case $ESCOLHA in' >> .listener-chat-again.sh
+echo '1)' >> .listener-chat-again.sh
+echo './listener-chat-interno.sh' >> .listener-chat-again.sh
+echo ';;' >> .listener-chat-again.sh
+echo '2)' >> .listener-chat-again.sh
+echo './listener-chat-externo.sh' >> .listener-chat-again.sh
+echo ';;' >> .listener-chat-again.sh
+echo '*)' >> .listener-chat-again.sh
+echo "./.listener-chat-again.sh" >> .listener-chat-again.sh
+echo ";;" >> .listener-chat-again.sh
+echo 'esac' >> .listener-chat-again.sh
+chmod +x .listener-chat-again.sh
+./.listener-chat-again.sh
+;;
+esac
