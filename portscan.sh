0f89a316d22ca5f2dc6741c5c99d62cdd30eb406
Eduardo <53496439+eduardbuzzi@users.noreply.github.com>
2020-03-29 23:14:10 -0300

    Update portscan.sh

--- portscan.sh
+++ portscan.sh
@@ -1,1 +1,35 @@
-
+#!/bin/bash
+chmod +x ./portscan-ip.sh
+chmod +x ./portscan-dominio.sh
+trap '' 2
+echo
+echo "(1) Scan das Portas de um IP (ex: 192.168.1.1)"
+echo "(2) Scan das Portas de um Dominio (ex: www.globo.com)"
+echo "(0) Voltar"
+echo
+read -p "Qual sua escolha? " ESCOLHA
+case $ESCOLHA in
+0) ./execute.sh ;;
+1) ./portscan-ip.sh ;;
+2) ./portscan-dominio.sh ;;
+*)
+echo "trap '' 2" > .portscan-again.sh
+echo "echo" >> .portscan-again.sh
+echo 'echo "(1) Scan das Portas de um IP (ex: 192.168.1.1)"' >> .portscan-again.sh
+echo 'echo "(2) Scan das Portas de um Dominio (ex: www.globo.com)"' >> .portscan-again.sh
+echo 'echo "(0) Voltar"' >> .portscan-again.sh
+echo 'echo' >> .portscan-again.sh
+echo 'read -p "Qual sua escolha? " ESCOLHA' >> .portscan-again.sh
+echo 'case $ESCOLHA in' >> .portscan-again.sh
+echo '0) ./execute.sh ;;' >> .portscan-again.sh
+echo '1) ./portscan-ip.sh ;;' >> .portscan-again.sh
+echo '2) ./portscan-dominio.sh ;;' >> .portscan-again.sh
+echo "*)" >> .portscan-again.sh
+echo "./.portscan-again.sh" >> .portscan-again.sh
+echo ";;" >> .portscan-again.sh
+echo 'esac' >> .portscan-again.sh
+chmod +x .portscan-again.sh
+./.portscan-again.sh
+;;
+esac
+exit
