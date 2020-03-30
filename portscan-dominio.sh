96c4505557aac01dca61a72fabe7bb9589f6ee51
Eduardo <53496439+eduardbuzzi@users.noreply.github.com>
2020-03-29 23:39:40 -0300

    Update portscan-dominio.sh

--- portscan-dominio.sh
+++ portscan-dominio.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo
 read -p "Informe o dominio: " DOMINIO
-IP=$(host $DOMINIO | cut -d ' ' -f4)
+IP=$(host $DOMINIO | cut -d ' ' -f4 | head -n1)
 if [ -z $IP ]
 then
 echo "O domínio $DOMINIO aparentemente está fora do ar!"
