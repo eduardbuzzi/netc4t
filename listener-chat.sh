c56aa914eb0328d0ff2f590470906dc5b3d2e496
Eduardo <53496439+eduardbuzzi@users.noreply.github.com>
2020-04-01 13:49:44 -0300

    Update listener-chat.sh

--- listener-chat.sh
+++ listener-chat.sh
@@ -5,8 +5,8 @@ IPexterno=$(grep "<title>" IP | cut -d ' ' -f9 | cut -d '<' -f1)
 IPinterno=$(hostname -I | cut -d " " -f1)
 rm IP
 echo
-echo "Seu IP Interno é: $IPinterno"
-echo "Seu IP Externo é: $IPexterno"
+echo -e "Seu IP Interno é: \033[01;31m$IPinterno\033[01;00m"
+echo -e "Seu IP Externo é: \033[01;31m$IPexterno\033[01;00m"
 echo
 read -p "Informe a porta para escutar (entre 1-65535): " PORTA
 trap 2
